e4353e110d0fdb5d28a129e229dc11e3d4f29484
pablorcruh <pablorcruh@gmail.com>
2019-07-22 17:02:59 -0500

    agregar RegisterActivity

---
+++ app/src/main/java/ec/com/pablorcruh/goodrecipes/activities/RegisterActivity.java
@@ -0,0 +1,15 @@
+package ec.com.pablorcruh.goodrecipes.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ec.com.pablorcruh.goodrecipes.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
