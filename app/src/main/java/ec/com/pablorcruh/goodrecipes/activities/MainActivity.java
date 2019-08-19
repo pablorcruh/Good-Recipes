795f8caeabb65a75b92ede6f93d6d292395e05fb
pablorcruh <pablorcruh@gmail.com>
2019-08-19 11:40:19 -0500

    arreglar problema en logout

--- app/src/main/java/ec/com/pablorcruh/goodrecipes/activities/MainActivity.java
+++ app/src/main/java/ec/com/pablorcruh/goodrecipes/activities/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnNavigationItemSelectedListener(navListener);
         if (savedInstanceState == null) {
